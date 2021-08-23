a220de4161a26a4bb7ae966e6b92ed60cf04a101
twobomb <stalkerfirst@mail.ru>
2021-08-23 22:53:23 +0300

    inlcude caldroid,  https://github.com/roomorama/Caldroid

--- app/src/main/java/com/twobomb/shifter/ui/home/HomeFragment.java
+++ app/src/main/java/com/twobomb/shifter/ui/home/HomeFragment.java
@@ -1,18 +1,27 @@
 package com.twobomb.shifter.ui.home;
 
+import android.app.Application;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.roomorama.caldroid.CaldroidFragment;
 import com.twobomb.shifter.R;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
@@ -20,16 +29,26 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        
+//        final CalendarView calendar = root.findViewById(R.id.calendar);
+        //calendar.
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
+        FragmentTransaction t = getActivity().getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar, caldroidFragment);
+        t.commit();
+
+
         return root;
     }
 }
