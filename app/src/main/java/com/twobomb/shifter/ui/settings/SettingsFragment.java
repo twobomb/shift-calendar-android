4cfa59a61b793b0538f146275c083543f2e8ab3b
twobomb <stalkerfirst@mail.ru>
2021-08-23 22:09:06 +0300

    Add settings

--- app/src/main/java/com/twobomb/shifter/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/twobomb/shifter/ui/settings/SettingsFragment.java
@@ -1,13 +1,19 @@
 package com.twobomb.shifter.ui.settings;
 
+import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -18,33 +24,148 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.twobomb.shifter.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SettingsFragment extends Fragment {
 
     private SettingsViewModel settingsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         settingsViewModel =
                 ViewModelProviders.of(this).get(SettingsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
         final Context ctx=  this.getActivity().getBaseContext();
         final Button btn_save = root.findViewById(R.id.btn_save);
         final Spinner s_groups = root.findViewById(R.id.s_groups);
+        final Switch sw_is_friday = root.findViewById(R.id.sw_is_friday);
+        final TimePicker tp_friday = root.findViewById(R.id.tp_fiday);
+        tp_friday.setIs24HourView(true);
+        final Switch sw_is_friday_repeat = root.findViewById(R.id.sw_is_friday_repeat);
+        final TimePicker tp_friday_repeat = root.findViewById(R.id.tb_friday_repeat);
+        tp_friday_repeat.setIs24HourView(true);
+        final Switch sw_auto_add_alarm = root.findViewById(R.id.sw_auto_add_alarm);
+        final TimePicker tp_alarm = root.findViewById(R.id.tp_alarm);
+        tp_alarm.setIs24HourView(true);
+        final Switch sw_notif_my_group = root.findViewById(R.id.sw_notif_my_group);
+        final TimePicker tp_notif_time = root.findViewById(R.id.tp_notif_time);
+        tp_notif_time.setIs24HourView(true);
+
+
+        s_groups.setSelection(settingsViewModel.getGroup().getValue());
+
+        sw_is_friday.setChecked(settingsViewModel.getIs_friday_remind().getValue());
+        tp_friday.setHour(settingsViewModel.getFriday_hour().getValue());
+        tp_friday.setMinute(settingsViewModel.getFriday_minute().getValue());
+
+        sw_is_friday_repeat.setChecked(settingsViewModel.getIs_friday_remind_repeat().getValue());
+        tp_friday_repeat.setHour(settingsViewModel.getFriday_repeat_hour().getValue());
+        tp_friday_repeat.setMinute(settingsViewModel.getFriday_repeat_minute().getValue());
+
+        sw_auto_add_alarm.setChecked(settingsViewModel.getIs_auto_add_alarm().getValue());
+        tp_alarm.setHour(settingsViewModel.getAlarm_hour().getValue());
+        tp_alarm.setMinute(settingsViewModel.getAlarm_minute().getValue());
+
+        sw_notif_my_group.setChecked(settingsViewModel.getIs_show_notification_my_group().getValue());
+        tp_notif_time.setHour(settingsViewModel.getNotif_hour().getValue());
+        tp_notif_time.setMinute(settingsViewModel.getNotif_minute().getValue());
+
+        //--------
+        sw_is_friday.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                settingsViewModel.setIs_friday_remind(b);
+            }
+        });
+
+
+        tp_friday.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                settingsViewModel.setFriday_hour(i);
+                settingsViewModel.setFriday_minute(i1);
+            }
+        });
+
+
+        //--------
+        sw_is_friday_repeat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                settingsViewModel.setIs_friday_remind_repeat(b);
+            }
+        });
+
+
+        tp_friday_repeat.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                settingsViewModel.setFriday_repeat_hour(i);
+                settingsViewModel.setFriday_repeat_minute(i1);
+            }
+        });
+
+        //--------
+        sw_auto_add_alarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                settingsViewModel.setIs_auto_add_alarm(b);
+            }
+        });
+
+
+        tp_alarm.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                settingsViewModel.setAlarm_hour(i);
+                settingsViewModel.setAlarm_minute(i1);
+            }
+        });
+        //--------
+        sw_notif_my_group.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                settingsViewModel.setIs_show_notification_my_group(b);
+            }
+        });
+        tp_notif_time.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                settingsViewModel.setNotif_hour(i);
+                settingsViewModel.setNotif_minute(i1);
+            }
+        });
+
+
+
+
+        s_groups.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                settingsViewModel.setGroup(i);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(ctx, "NothingSelected!" , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                settingsViewModel.Save();
                 Toast.makeText(ctx, "Сохранено!" , Toast.LENGTH_SHORT).show();
             }
         });
-        settingsViewModel.getGroup().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                String selected = getResources().getStringArray(R.array.shift_groups)[s_groups.getSelectedItemPosition()];
-                Toast.makeText(ctx, "Выбранно  " + selected, Toast.LENGTH_SHORT).show();
 
+
+        settingsViewModel.getGroup().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable Integer s) {
+                s_groups.setSelection(s);
             }
         });
         return root;
--- app/src/main/java/com/twobomb/shifter/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/twobomb/shifter/ui/settings/SettingsViewModel.java
@@ -1,19 +1,187 @@
 package com.twobomb.shifter.ui.settings;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SettingsViewModel extends ViewModel {
+public class SettingsViewModel extends AndroidViewModel {
 
-    private MutableLiveData<String> currentGroup;
+    private MutableLiveData<Integer> currentGroup;//группв
+    private MutableLiveData<Boolean> is_friday_remind;//Напоминать в пятницу
+    private MutableLiveData<Integer> friday_hour;//час
+    private MutableLiveData<Integer> friday_minute;//минуты
 
-    public SettingsViewModel() {
+    private MutableLiveData<Boolean> is_friday_remind_repeat;//повторять напоминать в пятницу
+    private MutableLiveData<Integer> friday_repeat_hour;//час
+    private MutableLiveData<Integer> friday_repeat_minute;//минуты
+
+    private MutableLiveData<Boolean> is_auto_add_alarm;//добавлять будильник на выходные
+    private MutableLiveData<Integer> alarm_hour;//час
+    private MutableLiveData<Integer> alarm_minute;//минуты
+
+    private MutableLiveData<Boolean> is_show_notification_my_group;//показывать уведомление если дежурит моя группа
+    private MutableLiveData<Integer> notif_hour;//час
+    private MutableLiveData<Integer> notif_minute;//минуты
+
+    private SharedPreferences sp;
+    public SettingsViewModel(final Application application) {
+        super(application);
         currentGroup = new MutableLiveData<>();
-        //currentGroup.setValue("This is tools fragment");
+        is_friday_remind = new MutableLiveData<>();
+        friday_hour = new MutableLiveData<>();
+        friday_minute = new MutableLiveData<>();
+        is_friday_remind_repeat = new MutableLiveData<>();
+        friday_repeat_hour = new MutableLiveData<>();
+        friday_repeat_minute = new MutableLiveData<>();
+        is_auto_add_alarm = new MutableLiveData<>();
+        alarm_hour = new MutableLiveData<>();
+        alarm_minute = new MutableLiveData<>();
+        is_show_notification_my_group = new MutableLiveData<>();
+        notif_hour = new MutableLiveData<>();
+        notif_minute = new MutableLiveData<>();
+
+
+         sp = getApplication().getApplicationContext().getSharedPreferences("APP_SETTINGS",Context.MODE_PRIVATE);
+        currentGroup.setValue(sp.getInt("group_index",0));
+        is_friday_remind.setValue(sp.getBoolean("is_friday_remind",true));
+        friday_hour.setValue(sp.getInt("friday_hour",10));
+        friday_minute.setValue(sp.getInt("friday_minute",0));
+        is_friday_remind_repeat.setValue(sp.getBoolean("is_friday_remind_repeat",true));
+        friday_repeat_hour.setValue(sp.getInt("friday_repeat_hour",14));
+        friday_repeat_minute.setValue(sp.getInt("friday_repeat_minute",30));
+        is_auto_add_alarm.setValue(sp.getBoolean("is_auto_add_alarm",true));
+        alarm_hour.setValue(sp.getInt("alarm_hour",7));
+        alarm_minute.setValue(sp.getInt("alarm_minute",45));
+        is_show_notification_my_group.setValue(sp.getBoolean("is_show_notification_my_group",true));
+        notif_hour.setValue(sp.getInt("notif_hour",7));
+        notif_minute.setValue(sp.getInt("notif_minute",0));
     }
 
-    public LiveData<String> getGroup() {
+    public LiveData<Integer> getGroup() {
         return currentGroup;
+    }
+    public void setGroup(Integer inx) {
+         currentGroup.setValue(inx);
+    }
+
+    public void Save(){
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("group_index",currentGroup.getValue());
+        editor.putBoolean("is_friday_remind",is_friday_remind.getValue());
+        editor.putInt("friday_hour",friday_hour.getValue());
+        editor.putInt("friday_minute",friday_minute.getValue());
+        editor.putBoolean("is_friday_remind_repeat",is_friday_remind_repeat.getValue());
+        editor.putInt("friday_repeat_hour",friday_repeat_hour.getValue());
+        editor.putInt("friday_repeat_minute",friday_repeat_minute.getValue());
+        editor.putBoolean("is_auto_add_alarm",is_auto_add_alarm.getValue());
+        editor.putInt("alarm_hour",alarm_hour.getValue());
+        editor.putInt("alarm_minute",alarm_minute.getValue());
+        editor.putBoolean("is_show_notification_my_group",is_show_notification_my_group.getValue());
+        editor.putInt("notif_hour",notif_hour.getValue());
+        editor.putInt("notif_minute",notif_minute.getValue());
+
+        editor.apply();
+    }
+
+
+    public LiveData<Boolean> getIs_friday_remind() {
+        return is_friday_remind;
+    }
+
+    public void setIs_friday_remind(Boolean val) {
+        this.is_friday_remind.setValue(val);
+    }
+
+    public MutableLiveData<Integer> getFriday_hour() {
+        return friday_hour;
+    }
+
+    public void setFriday_hour(Integer hour) {
+        this.friday_hour.setValue(hour);
+    }
+
+    public MutableLiveData<Integer> getFriday_minute() {
+        return friday_minute;
+    }
+
+    public void setFriday_minute(Integer minute) {
+        this.friday_minute.setValue(minute);
+    }
+
+    public MutableLiveData<Boolean> getIs_friday_remind_repeat() {
+        return is_friday_remind_repeat;
+    }
+
+    public void setIs_friday_remind_repeat(Boolean val) {
+        this.is_friday_remind_repeat.setValue(val);
+    }
+
+    public MutableLiveData<Integer> getFriday_repeat_hour() {
+        return friday_repeat_hour;
+    }
+
+    public void setFriday_repeat_hour(Integer friday_repeat_hour) {
+        this.friday_repeat_hour.setValue(friday_repeat_hour);
+    }
+
+    public MutableLiveData<Integer> getFriday_repeat_minute() {
+        return friday_repeat_minute;
+    }
+
+    public void setFriday_repeat_minute(Integer friday_repeat_minute) {
+        this.friday_repeat_minute.setValue(friday_repeat_minute);
+    }
+
+    public MutableLiveData<Boolean> getIs_auto_add_alarm() {
+        return is_auto_add_alarm;
+    }
+
+    public void setIs_auto_add_alarm(Boolean is_auto_add_alarm) {
+        this.is_auto_add_alarm.setValue(is_auto_add_alarm);
+    }
+
+    public MutableLiveData<Integer> getAlarm_hour() {
+        return alarm_hour;
+    }
+
+    public void setAlarm_hour(Integer alarm_hour) {
+        this.alarm_hour.setValue(alarm_hour);
+    }
+
+    public MutableLiveData<Integer> getAlarm_minute() {
+        return alarm_minute;
+    }
+
+    public void setAlarm_minute(Integer alarm_minute) {
+        this.alarm_minute.setValue(alarm_minute);
+    }
+
+    public MutableLiveData<Boolean> getIs_show_notification_my_group() {
+        return is_show_notification_my_group;
+    }
+
+    public void setIs_show_notification_my_group(Boolean val) {
+        this.is_show_notification_my_group.setValue(val);
+    }
+
+    public MutableLiveData<Integer> getNotif_hour() {
+        return notif_hour;
+    }
+
+    public void setNotif_hour(Integer hour) {
+        this.notif_hour.setValue(hour);
+    }
+
+    public MutableLiveData<Integer> getNotif_minute() {
+        return notif_minute;
+    }
+
+    public void setNotif_minute(Integer minute) {
+        this.notif_minute.setValue(minute);
     }
 }
